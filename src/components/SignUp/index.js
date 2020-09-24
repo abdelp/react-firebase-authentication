d8770c02275f94a4697e312369b0a24be7e39101
Abdel <juniorperezpy@gmail.com>
2020-09-24 09:14:42 -0400

    Refactor SignUpForm class component to a functional component

--- src/components/SignUp/index.js
+++ src/components/SignUp/index.js
@@ -1,9 +1,47 @@
-import React from 'react';
- 
-const SignUp = () => (
+import React, { Component, useState } from 'react';
+import { Link } from 'react-router-dom';
+
+import * as ROUTES from '../../constants/routes';
+
+const SignUpPage = () => (
   <div>
     <h1>SignUp</h1>
+    <SignUpForm />
   </div>
 );
- 
-export default SignUp;
+
+const INITIAL_STATE = {
+  username: '',
+  email: '',
+  password: '',
+  passwordConfirmation: '',
+  error: null,
+};
+
+function SignUpForm(props) {
+  const [state, setState] = useState({...INITIAL_STATE});
+
+  const onSubmit = event => {
+
+  }
+
+  const onChange = event => {
+
+  }
+
+  return (
+    <form onSubmit={onSubmit}>
+
+    </form>
+  );
+}
+
+const SignUpLink = () => (
+  <p>
+    Don't have an account? <Link to={ROUTES.SIGN_UP}>Sign Up</Link>
+  </p>
+);
+
+export default SignUpPage;
+
+export { SignUpForm, SignUpLink };
