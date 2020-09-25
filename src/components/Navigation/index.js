e7f8122d80d2e17d6435d60175bcf53d5fff5669
Abdel <juniorperezpy@gmail.com>
2020-09-25 19:43:04 -0400

    Add implementation of signin and signout

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -4,26 +4,36 @@ import { Link } from 'react-router-dom';
 import SignOutButton from '../SignOut';
 import * as ROUTES from '../../constants/routes';
 
-const Navigation = () => (
+const Navigation = ({ authUser }) => {
+  return (
+  <div>{authUser ? <NavigationAuth /> : <NavigationNonAuth />}</div>
+)};
+
+const NavigationAuth = () => (
+  <ul>
+    <li>
+      <Link to={ROUTES.LANDING}>Landing</Link>
+    </li>
+    <li>
+      <Link to={ROUTES.HOME}>Home</Link>
+    </li>
+    <li>
+      <Link to={ROUTES.ACCOUNT}>Account</Link>
+    </li>
+    <li>
+      <SignOutButton />
+    </li>
+  </ul>
+);
+
+const NavigationNonAuth = () => (
   <div>
     <ul>
-      <li>
-        <Link to={ROUTES.SIGN_IN}>Sign In</Link>
-      </li>
       <li>
         <Link to={ROUTES.LANDING}>Landing</Link>
       </li>
       <li>
-        <Link to={ROUTES.HOME}>Home</Link>
-      </li>
-      <li>
-        <Link to={ROUTES.ACCOUNT}>Account</Link>
-      </li>
-      <li>
-        <Link to={ROUTES.ADMIN}>Admin</Link>
-      </li>
-      <li>
-        <SignOutButton />
+        <Link to={ROUTES.SIGN_IN}>Sign In</Link>
       </li>
     </ul>
   </div>
