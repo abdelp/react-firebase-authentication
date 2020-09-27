586c7c964073780c50546b55ba4276a7af0ca351
Abdel <juniorperezpy@gmail.com>
2020-09-27 18:08:32 -0400

    Add update password feature and add it to the account page with the update password

--- src/components/PasswordChange/index.js
+++ src/components/PasswordChange/index.js
@@ -1,9 +1,61 @@
-import React from 'react';
+import React, { Component, useState } from 'react';
+import { withFirebase } from '../Firebase';
+
+const INITIAL_STATE = {
+  password: '',
+  passwordConfirmation: '',
+  error: null,
+};
+
+const PasswordChangeForm = (props) => {
+  const [state, setState] = useState({...INITIAL_STATE});
+
+  const onSubmit = event => {
+    const { password } = state;
+
+    props
+      .firebase
+      .updatePassword(password)
+      .then(() =>
+        setState(state => ({ ...INITIAL_STATE}))
+      )
+      .catch(error =>
+        setState(state => ({...state, error}))
+      );
+
+    event.preventDefault();
+  };
+
+  const onChange = event =>
+    setState(state => ({...state, [event.target.name]: event.target.value }));
+
+  const isInvalid =
+    state.password !== state.passwordConfirmation ||
+    !state.password;
+
+  return (
+    <form onSubmit={onSubmit}>
+      <input
+        name="password"
+        value={state.password}
+        onChange={onChange}
+        type="password"
+        placeholder="New Password"
+      />
+      <input
+        name="passwordConfirmation"
+        value={state.passwordConfirmation}
+        onChange={onChange}
+        type="password"
+        placeholder="Confirm New Password"
+      />
+      <button disabled={isInvalid} type="submit">
+        Reset My Password
+      </button>
+
+      {state.error && <p>{state.error.message}</p>}
+    </form>
+  );
+};
  
-const PasswordChange = () => (
-  <div>
-    <h1>PasswordChange</h1>
-  </div>
-);
- 
-export default PasswordChange;
+export default withFirebase(PasswordChangeForm);
