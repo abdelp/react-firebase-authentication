fb31dd4e0bc905774b8c192077769d7b7a3313ac
Abdel <juniorperezpy@gmail.com>
2020-10-06 18:56:17 -0300

    Update FirebaseContext value to use a functional component instead of a class component

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import * as serviceWorker from './serviceWorker';
 import Firebase, { FirebaseContext } from './components/Firebase';
 
 ReactDOM.render(
-  <FirebaseContext.Provider value={new Firebase()}>
+  <FirebaseContext.Provider value={Firebase()}>
     <App />
   </FirebaseContext.Provider>,
   document.getElementById('root')
