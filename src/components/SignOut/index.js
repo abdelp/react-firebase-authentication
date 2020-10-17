a1f5627c4e02cfcb4c24515f700e6cda065130d0
Abdel <juniorperezpy@gmail.com>
2020-10-17 10:54:39 -0300

    Add PropTypes to SignOut component

--- src/components/SignOut/index.js
+++ src/components/SignOut/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import PropTypes from 'prop-types';
 import { withFirebase } from '../Firebase';
 
 const SignOutButton = ({ firebase }) => (
@@ -7,5 +7,11 @@ const SignOutButton = ({ firebase }) => (
     Sign Out
   </button>
 );
+
+SignOutButton.propTypes = {
+  firebase: PropTypes.shape({
+    signOut: PropTypes.func,
+  }).isRequired,
+};
 
 export default withFirebase(SignOutButton);
