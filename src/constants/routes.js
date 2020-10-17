46ffc588e8169efda5a04b6e54396768b0f11e12
Abdel <juniorperezpy@gmail.com>
2020-10-17 14:25:53 -0300

    Add ADMIN_DETAILS route

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -3,5 +3,6 @@ export const SIGN_UP = '/signup';
 export const SIGN_IN = '/signin';
 export const HOME = '/home';
 export const ACCOUNT = '/account';
-export const ADMIN = '/admin';
 export const PASSWORD_FORGET = '/pw-forget';
+export const ADMIN = '/admin';
+export const ADMIN_DETAILS = '/admin/:id';
