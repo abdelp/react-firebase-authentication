e6de5e2f8ee065d68c62c5617797597f7444bb2f
Abdel <juniorperezpy@gmail.com>
2020-10-17 21:49:46 -0300

    Update Home component to use email verificator

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { withAuthorization } from '../Session';
+import { compose } from 'recompose';
+
+import { withAuthorization, withEmailVerification } from '../Session';
 
 const HomePage = () => (
   <div>
@@ -9,4 +11,7 @@ const HomePage = () => (
 
 const condition = authUser => !!authUser;
 
-export default withAuthorization(condition)(HomePage);
+export default compose(
+  withEmailVerification,
+  withAuthorization(condition),
+)(HomePage);
