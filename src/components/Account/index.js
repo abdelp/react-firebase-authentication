b587e3312d72fce8ef5c4c0d67ddb374dab634d6
Abdel <juniorperezpy@gmail.com>
2020-10-17 21:45:52 -0300

    Add email verificator for Account page

--- src/components/Account/index.js
+++ src/components/Account/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
+import { compose } from 'recompose';
 
-import { AuthUserContext, withAuthorization } from '../Session';
+import {
+  AuthUserContext,
+  withAuthorization,
+  withEmailVerification,
+} from '../Session';
 import { PasswordForgetForm } from '../PasswordForget';
 import PasswordChangeForm from '../PasswordChange';
 
@@ -21,4 +26,7 @@ const AccountPage = () => (
 
 const condition = authUser => !!authUser;
 
-export default withAuthorization(condition)(AccountPage);
+export default compose(
+  withEmailVerification,
+  withAuthorization(condition),
+)(AccountPage);
