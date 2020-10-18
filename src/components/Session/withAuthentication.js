076c2d930c2b220b265a18773d74b19301c3bd0b
Abdel <juniorperezpy@gmail.com>
2020-10-17 21:53:56 -0300

    Add PropTypes for withAuthentication

--- src/components/Session/withAuthentication.js
+++ src/components/Session/withAuthentication.js
@@ -1,16 +1,21 @@
 import React, { useState, useEffect } from 'react';
-
+import PropTypes from 'prop-types';
 import AuthUserContext from './context';
 import { withFirebase } from '../Firebase';
 
 const withAuthentication = Component => {
   const WithAuthentication = props => {
-    const [state, setState] = useState({ authUser: null });
-    const [listener, setListener] = useState();
+    const [state, setState] = useState({
+      authUser: JSON.parse(localStorage.getItem('authUser')),
+    });
 
     useEffect(() => {
-      const authListener = props.firebase.auth.onAuthStateChanged(
+      const authListener = props.firebase.onAuthUserListener(
         authUser => setState(state => ({ ...state, authUser })),
+        () => {
+          localStorage.removeItem('authUser');
+          setState({ authUser: null });
+        },
       );
       return () => authListener();
     }, []);
@@ -20,6 +25,15 @@ const withAuthentication = Component => {
         <Component {...props} />
       </AuthUserContext.Provider>
     );
+  };
+
+  WithAuthentication.propTypes = {
+    firebase: PropTypes.shape({
+      auth: PropTypes.shape({
+        onAuthStateChanged: PropTypes.func,
+      }),
+      onAuthUserListener: PropTypes.func,
+    }).isRequired,
   };
 
   return withFirebase(WithAuthentication);
