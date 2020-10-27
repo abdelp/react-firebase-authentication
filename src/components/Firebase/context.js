38e7295c75d91eb4eb362321ef3745cbdb379c0e
Abdel <juniorperezpy@gmail.com>
2020-10-27 19:30:59 -0300

    Disable eslint jsx-props-no-spreading and react/display-name cops

--- src/components/Firebase/context.js
+++ src/components/Firebase/context.js
@@ -2,6 +2,8 @@ import React from 'react';
 
 const FirebaseContext = React.createContext(null);
 
+/* eslint-disable react/jsx-props-no-spreading */
+// eslint-disable-next-line react/display-name
 export const withFirebase = Component => props => (
   <FirebaseContext.Consumer>
     {firebase => <Component {...props} firebase={firebase} />}
