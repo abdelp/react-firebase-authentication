b4a57d32c031f095093c3f5c5a205ed49aac27ce
Abdel <juniorperezpy@gmail.com>
2020-10-27 19:23:11 -0300

    Move down styling files and remove comments from index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
 
 import Firebase, { FirebaseContext } from './components/Firebase';
 
@@ -13,7 +16,4 @@ ReactDOM.render(
   document.getElementById('root'),
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
